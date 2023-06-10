327d357b9e2e53fa679e08bb8ca7101216da0ac4
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2023-06-10 02:28:56 +0200

    feat: about me section

--- src/app/page.js
+++ src/app/page.js
@@ -51,6 +51,17 @@ export default function Home() {
           </ul>
         </header>
         <content className="pt-24 lg:w-1/2 lg:py-24">
+          <section id="about" className="mb-16 scroll-mt-16 text-zinc-500 md:mb-24 lg:mb-36 lg:scroll-mt-24">
+            <p className="mb-4">
+              With 5 years of professional experience as a software engineer, my focus lies in frontend web development. I have successfully collaborated with both large and small teams, consistently delivering modern solutions by staying abreast of the latest technologies.
+            </p>
+            <p className="mb-4">
+              As an entrepreneur, I have had the opportunity to work closely with startups, leveraging my coding and design expertise to contribute to their success. Throughout these experiences, I have honed my skills in creating compelling and user-friendly interfaces for web applications.
+            </p>
+            <p>
+              I have a true passion for coding, design, and continuous learning. These interests drive me to stay ahead of industry trends and seek out new opportunities for growth and improvement. With a strong foundation in frontend development and a drive for innovation, I am eager to tackle new challenges and contribute to impactful projects.
+            </p>
+          </section>
           <section id="experiences">
             {
               experiencesData.map(exp => {
