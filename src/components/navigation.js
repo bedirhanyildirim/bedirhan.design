6010942ad5fc8070ba5248c02bdaee290d3248df
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2023-07-13 00:09:48 +0200

    feat: highlight active nav item

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,4 +1,26 @@
-export default function Navigation({aboutRef, experiencesRef, educationRef}) {
+import { useEffect, useState } from 'react'
+import useIsInViewport from '@/hooks/useIsInViewport'
+
+export default function Navigation({ aboutRef, experiencesRef, educationRef }) {
+  const [activeNavigationItem, setActiveNavigationItem] = useState(null)
+  
+  const isAboutInViewport = useIsInViewport(aboutRef)
+  const isExperiencesInViewport = useIsInViewport(experiencesRef)
+  const isEducationInViewport = useIsInViewport(educationRef)
+  
+  const getNavigationClasses = (sectionName) => {
+    return sectionName === activeNavigationItem ? 'text-xs font-bold uppercase tracking-widest text-sky-800' : 'text-xs font-bold uppercase tracking-widest text-zinc-600 group-hover:text-sky-800 group-focus-visible:text-sky-800'
+  }
+  
+  useEffect(() => {
+    if (isAboutInViewport === true) {
+      setActiveNavigationItem('about')
+    } else if (isEducationInViewport === true) {
+      setActiveNavigationItem('education')
+    } else if (isExperiencesInViewport === true) {
+      setActiveNavigationItem('experiences')
+    }
+  }, [isAboutInViewport, isExperiencesInViewport, isEducationInViewport])
   
   const scrollToAbout = () => {
     aboutRef.current.scrollIntoView({behavior: "smooth"})
@@ -15,9 +37,9 @@ export default function Navigation({aboutRef, experiencesRef, educationRef}) {
       <li>
         <div
           onClick={scrollToAbout}
-          className="group flex items-center py-3 hover:cursor-pointer"
+          className="group flex items-center py-3 hover:cursor-pointer text-red-600"
         >
-          <span className="text-xs font-bold uppercase tracking-widest text-zinc-600 group-hover:text-sky-800 group-focus-visible:text-sky-800">
+          <span className={getNavigationClasses('about')}>
             About
           </span>
         </div>
@@ -27,7 +49,7 @@ export default function Navigation({aboutRef, experiencesRef, educationRef}) {
           onClick={scrollToExperiences}
           className="group flex items-center py-3 hover:cursor-pointer"
         >
-          <span className="text-xs font-bold uppercase tracking-widest text-zinc-600 group-hover:text-sky-800 group-focus-visible:text-sky-800">
+          <span className={getNavigationClasses('experiences')}>
             Experiences
           </span>
         </div>
@@ -37,7 +59,7 @@ export default function Navigation({aboutRef, experiencesRef, educationRef}) {
           onClick={scrollToEducation}
           className="group flex items-center py-3 hover:cursor-pointer"
         >
-          <span className="text-xs font-bold uppercase tracking-widest text-zinc-600 group-hover:text-sky-800 group-focus-visible:text-sky-800">
+          <span className={getNavigationClasses('education')}>
             Education
           </span>
         </div>
