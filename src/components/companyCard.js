ad76c0f3126600e820c15ab05466152ccb8d1fe3
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2023-07-23 14:01:23 +0200

    feat: create animation

--- src/components/companyCard.js
+++ src/components/companyCard.js
@@ -1,9 +1,25 @@
 import Link from 'next/link'
 import ArrowOut from 'public/arrowOut.svg'
+import { useEffect, useRef } from 'react'
+import useIsInViewport from '@/hooks/useIsInViewport'
 
 export default function CompanyCard({ experience }) {
+  
+  const companyCardRef = useRef()
+  const isCompanyCardInViewport = useIsInViewport(companyCardRef)
+  
+  useEffect(() => {
+    if (isCompanyCardInViewport) {
+      const element = companyCardRef.current
+      if (!element?.className.includes('card-animation')) {
+        element.className += ' card-animation'
+        element.style.animationDelay = `${experience.id}s`
+      }
+    }
+  }, [isCompanyCardInViewport])
+  
   return (
-    <div className="mb-12 relative group hover:cursor-default">
+    <div ref={companyCardRef} className="mb-12 relative group hover:cursor-default">
       <div className="absolute -inset-x-4 -inset-y-4 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-gray-200/20 lg:group-hover:shadow-[inset_0_0_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
       <div className="relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:gap-10 xl:gap-2">
         <header className="mb-2 md:mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 col-span-2 whitespace-nowrap">
