11bece0408d0f8d2b5770de47257b34d6919909d
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2023-08-29 11:01:14 +0200

    fix: hire me banner gap

--- src/components/bannerCard.js
+++ src/components/bannerCard.js
@@ -4,8 +4,8 @@ import UpworkLogo from '../../public/upworkLogo.svg'
 export default function BannerCard() {
   return (
     <div className="group w-full lg:w-10/12 py-6 px-5 transition-all relative gap-4 flex items-center cursor-pointer rounded-md bg-gray-100 dark:bg-zinc-800 lg:hover:bg-gray-200/20 dark:lg:hover:bg-zinc-600/10">
-      <div className="w-full flex flex-col gap-1">
-        <span className="text-xs font-semibold uppercase tracking-wide text-slate-500 dark:text-slate-400 col-span-2 font-mono whitespace-nowrap">You can hire me</span>
+      <div className="w-full flex flex-col gap-2">
+        <span className="text-xs font-semibold uppercase tracking-wide text-slate-500 dark:text-slate-400 col-span-2 whitespace-nowrap">You can hire me</span>
         <h3 className="text-lg capitalize leading-tight text-zinc-800 dark:text-zinc-200 text-base">
           Let’s work together on your project!
         </h3>
