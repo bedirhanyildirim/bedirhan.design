2d4ed6e35c3e62f55ec290417e8997c42a79af90
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2024-01-16 00:14:53 +0100

    fix: delete old project

--- src/components/footerMobile.js
+++
@@ -1,7 +0,0 @@
-export default function FooterMobile() {
-  return (
-    <div className="w-full flex justify-center py-3 text-xs text-gray-700 dark:text-gray-300 border-t dark:border-gray-700 lg:hidden">
-      developed by <span className="font-bold inline-block ml-1">@bedcodes</span>
-    </div>
-  )
-}
