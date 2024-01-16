8cc4f290fce90932c30d814c56f18d4000d85697
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2024-01-16 21:53:04 +0100

    feat: add soon alert to blog page

--- app/(default)/(routes)/blog/page.tsx
+++ app/(default)/(routes)/blog/page.tsx
@@ -1,3 +1,16 @@
-export default function AboutMe() {
-  return <>AboutMe</>;
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertCircle } from "lucide-react";
+
+export default function BlogPage() {
+  return (
+    <>
+      <Alert variant="default">
+        <AlertCircle className="h-4 w-4" />
+        <AlertTitle>Development continuous</AlertTitle>
+        <AlertDescription>
+          Blog page is going to be here soon.
+        </AlertDescription>
+      </Alert>
+    </>
+  );
 }
