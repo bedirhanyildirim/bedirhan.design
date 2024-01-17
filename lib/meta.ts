1740d0fe213f3b2b7bf110ef9e1fe870a2cdbe1a
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2024-01-17 21:36:53 +0100

    feat: google tag manager added

---
+++ lib/meta.ts
@@ -0,0 +1,17 @@
+export const META = {
+  title: "Bedirhan Design | Portfolio",
+  description: 'Welcome to Bedirhan "bedcodes" Yildirim projects portfolio',
+  url: "https://bedirhan.design",
+  email: "bedcodes@gmail.com",
+  name: "Bedirhan Yildirim",
+  social: {
+    instagram: "https://instagram.com/bedcodes",
+    twitter: "https://twitter.com/bedcodes",
+    website: "https://bedirhanyildirim.com",
+    github: "https://github.com/bedirhanyildirim",
+    superpeer: "https://superpeer.com/bedirhan",
+  },
+};
+
+export const GA_ID = "G-RX1552YQE9";
+export const GTM_ID = "GTM-K6F5S53";
