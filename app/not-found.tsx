1a8206d2bd558b8014551e5c1bd6fcc0df56a62d
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2024-01-18 22:54:23 +0100

    fix: 404 page special char

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,7 +8,7 @@ export default function NotFoundPage() {
         404 Not Found
       </h1>
       <p className="text-lg text-gray-600 dark:text-gray-400 mt-4">
-        The page you're looking for doesn't exist.
+        The page you are looking for does not exist.
       </p>
       <Link
         className="mt-8 inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
